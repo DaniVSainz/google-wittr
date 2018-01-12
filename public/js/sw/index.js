d1bd6fb071d4924c0996752ae84f6d7a5b93c4f3
danivsainz <danivsainz@gmail.com>
2018-01-12 09:42:19 -0500

    Running git reset next

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,30 +1,9 @@
+self.addEventListener('install', function(){
+  
+})
+
+
 self.addEventListener('fetch', function(event) {
-  // event.respondWith(
-  //   new Response(event, {
-  //     headers: {'Content-Type': 'text/html'}
-  //   })
-  // );
-
-  // if (event.request.destination == 'image'){
-  //   event.respondWith(
-  //    fetch('/imgs/dr-evil.gif')
-  //    );
-  // }
-  // event.respondWith(
-  //   fetch('/imgs/dr-evil.gif')
-  // );
-  // console.log(event.request.destination);
-  // console.log(event.request)
-  // var length = event.request.url.length
-  // var urlEnding = event.request.url.substring(length-4, length);
-  // console.log(urlEnding)
-
-  //   if (urlEnding == '.jpg'){
-  //   event.respondWith(
-  //    fetch('/imgs/dr-evil.gif')
-  //    );
-  // }
-
   event.respondWith(
     fetch(event.request).then(function(response){
       if (response.status == 404){
