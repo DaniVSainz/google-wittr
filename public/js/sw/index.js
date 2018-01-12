0fd6b0ac3ff75a6f4f106ef061d36eebaac6b035
danivsainz <danivsainz@gmail.com>
2018-01-12 17:29:44 -0500

    Now allowing users to update service worker via a refresh button

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -40,3 +40,12 @@ self.addEventListener('fetch', function(event) {
 
 // TODO: listen for the "message" event, and call
 // skipWaiting if you get the appropriate message
+self.addEventListener('message', function(event){
+  if ( event.data.action == "skipWaiting"){
+    self.skipWaiting();
+  }
+})
+//asdasd
+//test
+//asd asdasdasdad
+//asdasdasdasdasd
