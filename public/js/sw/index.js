bf5d63b9ef3e0a5fff856e58eeed13f3999f39f2
danivsainz <danivsainz@gmail.com>
2018-01-12 09:59:34 -0500

    Now caching an array of urls

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,18 +1,23 @@
-self.addEventListener('install', function(){
-  
-})
+self.addEventListener('install', function(event) {
+  var urlsToCache = [
+    '/',
+    'js/main.js',
+    'css/main.css',
+    'imgs/icon.png',
+    'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+    'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+  ];
 
+  event.waitUntil(
+    // TODO: open a cache named 'wittr-static-v1'
+    // Add cache the urls from urlsToCache
+    caches.open('wittr-static-v1').then(function(cache){
+      cache.addAll(urlsToCache)
+    })
+  );
+});
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    fetch(event.request).then(function(response){
-      if (response.status == 404){
-        // return new Response('Whoops, not found');
-        return  fetch('/imgs/dr-evil.gif')
-      }
-      return response;
-    }).catch(function(){
-      return new Response("Our site is broken, sorry");
-    })
-  )
+  // Leave this blank for now.
+  // We'll get to this in the next task.
 });
